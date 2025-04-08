69f463ee0280989a70099262dc774802e1b7e3ac
janghye0k <d0oR.hyeok@gmail.com>
2025-04-08 14:25:03 +0900

    fix: build filename and export name

--- rollup.config.js
+++ rollup.config.js
@@ -3,22 +3,18 @@ import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: 'index.js', // 엔트리 파일
+  input: 'index.js',
   output: [
     {
-      file: 'dist/index.cjs.js', // CommonJS 출력
+      file: 'dist/index.cjs',
       format: 'cjs',
       exports: 'auto',
     },
     {
-      file: 'dist/index.esm.js', // ESM 출력
+      file: 'dist/index.mjs',
       format: 'esm',
     },
   ],
   external: ['prettier', 'prettier-plugin-tailwindcss'], // peerDependencies는 외부로 설정
-  plugins: [
-    nodeResolve(), // Node.js 모듈 해석
-    commonjs(), // CommonJS 모듈 변환
-    terser(), // 코드 압축
-  ],
+  plugins: [nodeResolve(), commonjs(), terser()],
 };
